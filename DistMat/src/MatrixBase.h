4276ad1194d3e1f7dd6da36158aca660a3fecb28
PascalZh <1315521905@qq.com>
2021-05-12 16:10:33 +0800

    Find out why even derived function is not defined but compiler will not error.

--- DistMat/src/MatrixBase.h
+++ DistMat/src/MatrixBase.h
@@ -11,23 +11,30 @@ private:
   const Derived& derived() const { return *static_cast<const Derived*>(this); }
 
 public:
-  template <typename Dest>
-  void evalTo(Dest& dst) const { derived().evalTo(dst); }
+
+#define	EnsureOverwritten(F)	static_assert(!std::is_same<decltype(&Derived::F),\
+        decltype(&MatrixBase<Derived>::F)>::value,\
+        "static polymorphic function " #F " was not overwritten.");
 
   template <typename Dest>
-  void addTo(Dest& dst) const { derived().addTo(dst); }
+  void evalTo(Dest& dst) const { derived().evalTo_impl(dst); }
 
   template <typename Dest>
-  void subTo(Dest& dst) const { derived().subTo(dst); }
+  void addTo(Dest& dst) const { derived().addTo_impl(dst); }
 
   template <typename Dest>
-  void mulLeftTo(Dest& dst) const { derived().mulLeftTo(dst); }
+  void subTo(Dest& dst) const { derived().subTo_impl(dst); }
 
   template <typename Dest>
-  void mulRightTo(Dest& dst) const { derived().mulRightTo(dst); }
+  void mulLeftTo(Dest& dst) const { derived().mulLeftTo_impl(dst); }
 
-  Index rows() const { return derived().rows(); }
-  Index cols() const { return derived().cols(); }
+  template <typename Dest>
+  void mulRightTo(Dest& dst) const { derived().mulRightTo_impl(dst); }
+
+  // When a Base::f calls the Derived::f, but Derived::f is not defined,
+  // compiler will not error, so add EnsureOverwritten to avoid this.
+  Index rows() const { EnsureOverwritten(rows); return derived().rows(); }
+  Index cols() const { EnsureOverwritten(cols); return derived().cols(); }
   Index size() const { return rows() * derived().cols(); }
 };
 
