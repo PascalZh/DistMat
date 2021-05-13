a9b650c4278209ab8a2eb11e495ea3c113fe7e92
PascalZh <1315521905@qq.com>
2021-05-13 16:38:23 +0800

    Working on MatrixBase.

--- DistMat/src/Matrix.h
+++ DistMat/src/Matrix.h
@@ -4,20 +4,34 @@
 
 namespace DistMat
 {
-template <typename Dtype>
+
+template <typename Dtype, bool _Lazy = true>
 class Matrix : public MatrixBase<Matrix<Dtype>> {
 public:
+  using value_type = Dtype;
+
   Matrix() = default;
-  Matrix(size_t rows, size_t cols)
-    : m_plain_object(rows * cols), m_rows(rows), m_cols(cols)
+  Matrix(Index rows, Index cols)
+    : m_plain_object(rows * cols), m_rows(rows), m_cols(cols) { }
+
+  const value_type& accessWithoutBoundsChecking(Index row, Index col) const
   {
+    // use C order instead of Fortran order
+    return m_plain_object[row * rows() + col];
   }
-  size_t rows() const { return m_rows; }
-  size_t cols() const { return m_cols; }
+
+  const value_type& accessWithBoundsChecking(Index row, Index col) const
+  {
+    // use C order instead of Fortran order
+    return m_plain_object.at(row * rows() + col);
+  }
+
+  Index rows() const { return m_rows; }
+  Index cols() const { return m_cols; }
 private:
-  std::vector<Dtype> m_plain_object;
-  size_t m_rows = 0;
-  size_t m_cols = 0;
+  std::vector<value_type> m_plain_object;
+  Index m_rows = 0;
+  Index m_cols = 0;
 };
 
 } // end of namespace DistMat
--- DistMat/src/MatrixBase.h
+++ DistMat/src/MatrixBase.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <iostream>
 namespace DistMat
 {
 using Index = size_t;
@@ -10,11 +10,35 @@ private:
   Derived& derived() { return *static_cast<Derived*>(this); }
   const Derived& derived() const { return *static_cast<const Derived*>(this); }
 
+  const Derived& const_derived() { return const_cast<const Derived&>(derived()); }
+  const Derived& const_derived() const { return *static_cast<const Derived*>(this); }
 public:
 
 #define	EnsureOverwritten(F)	static_assert(!std::is_same<decltype(&Derived::F),\
         decltype(&MatrixBase<Derived>::F)>::value,\
         "static polymorphic function " #F " was not overwritten.");
+
+  // Derived only need to define const version of accessWithoutBoundsChecking()
+  Derived::value_type& operator()(Index row, Index col)
+  {
+    const_cast<Derived::value_type&>(
+      const_derived().accessWithoutBoundsChecking(row, col)
+    );
+  }
+  const Derived::value_type& operator()(Index row, Index col) const
+  {
+    const_derived().accessWithoutBoundsChecking(row, col);
+  }
+  Derived::value_type& at(Index row, Index col)
+  {
+    const_cast<Derived::value_type&>(
+      const_derived().accessWithBoundsChecking(row, col)
+    );
+  }
+  const Derived::value_type& at(Index row, Index col) const
+  {
+    const_derived().accessWithBoundsChecking(row, col);
+  }
 
   template <typename Dest>
   void evalTo(Dest& dst) const { derived().evalTo_impl(dst); }
@@ -32,10 +56,52 @@ public:
   void mulRightTo(Dest& dst) const { derived().mulRightTo_impl(dst); }
 
   // When a Base::f calls the Derived::f, but Derived::f is not defined,
-  // compiler will not error, so add EnsureOverwritten to avoid this.
+  // compiler will not error, add EnsureOverwritten to avoid this.
   Index rows() const { EnsureOverwritten(rows); return derived().rows(); }
   Index cols() const { EnsureOverwritten(cols); return derived().cols(); }
   Index size() const { return rows() * derived().cols(); }
+
+  template <typename OtherDerived>
+  MatrixBase<Derived>& operator=(const MatrixBase<OtherDerived>& other)
+  {
+    other.evalTo(derived());
+  }
+
+  template <typename OtherDerived>
+  MatrixBase<Derived>& operator+=(const MatrixBase<OtherDerived>& other)
+  {
+    other.addTo(derived());
+  }
+
+  template <typename OtherDerived>
+  MatrixBase<Derived>& operator-=(const MatrixBase<OtherDerived>& other)
+  {
+    other.subTo(derived());
+  }
 };
+
+template <typename Dest, typename Derived>
+Dest operator+(const Dest& lhs, const MatrixBase<Derived>& rhs)
+{
+  Dest tmp = lhs;
+  rhs.addTo(tmp);
+  return tmp;
+}
+
+template <typename Dest, typename Derived>
+Dest operator-(const Dest& lhs, const MatrixBase<Derived>& rhs)
+{
+  Dest tmp = lhs;
+  rhs.subTo(tmp);
+  return tmp;
+}
+
+template <typename Dest, typename Derived>
+Dest operator*(const Dest& lhs, const MatrixBase<Derived>& rhs)
+{
+  Dest tmp = lhs;
+  rhs.mulRightTo(tmp);
+  return tmp;
+}
 
 } // end of namespace DistMat
